1cf6957019588bbccca3764dd0ed7110ae58484a
crevulus <crevulus@gmail.com>
2020-07-10 20:16:29 +0200

    finished cookie consent styling

--- price-comparison-frontend/src/App.js
+++ price-comparison-frontend/src/App.js
@@ -9,7 +9,20 @@ function App() {
     <div className="App">
       <Navbar />
       <SidePanel />
-      <CookieConsent />
+      <CookieConsent
+        enableDeclineButton
+        buttonText="Accept"
+        buttonStyle={{ backgroundColor: "#009785", color: "white" }}
+        declineButtonText="Reject"
+        declineButtonStyle={{
+          backgroundColor: "#FFC749",
+          color: "#000000",
+        }}
+        overlay
+      >
+        This website uses cookies to enhance user experience. Cookies will be
+        used for analytics, personalised content, and third-party tracking.
+      </CookieConsent>
     </div>
   );
 }
