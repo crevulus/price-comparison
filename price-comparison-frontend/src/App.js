14eb48b63b7575f29265a2cd2f2626fda01787eb
crevulus <crevulus@gmail.com>
2020-07-10 19:18:35 +0200

    cookie consent

--- price-comparison-frontend/src/App.js
+++ price-comparison-frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import SidePanel from "./components/SidePanel";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SidePanel />
+      <CookieConsent />
     </div>
   );
 }
---
+++ price-comparison-frontend/src/components/SidePanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class SidePanel extends Component {
+  render() {
+    return (
+      <div className="sidenav">
+        <span>Test</span>
+      </div>
+    );
+  }
+}
+
+export default SidePanel;
