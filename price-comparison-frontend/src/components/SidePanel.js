1be50b88f7ba31df3973bc9a69c7df0c46c1d953
crevulus <crevulus@gmail.com>
2020-07-15 15:42:28 +0200

    data updating (hardcoded values) on button click

--- price-comparison-frontend/src/components/SidePanel.js
+++ price-comparison-frontend/src/components/SidePanel.js
@@ -1,32 +1,92 @@
 import React, { Component } from "react";
-import Graph from "./Graph";
+import Chart from "chart.js";
 import dummyData from "../react-dummy_form_answer.json";
 
 export class SidePanel extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      chart: {},
+    };
+    this.chartRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(dummyData);
+    const myChart = new Chart(this.chartRef.current, {
+      type: "bar",
+      data: {
+        labels: ["Change=", "Competitor"],
+        datasets: [
+          {
+            label: "Service costs",
+            backgroundColor: "pink",
+            data: [, 175],
+          },
+          {
+            label: "Base rent",
+            backgroundColor: "lightyellow",
+            data: [600, 650],
+          },
+          {
+            label: "Add ons",
+            backgroundColor: "lightgreen",
+            data: [80, 120],
+          },
+          {
+            label: "Water",
+            backgroundColor: "lightblue",
+            data: [30, 30],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+          mode: "nearest",
+        },
+        scales: {
+          xAxes: [
+            {
+              stacked: true,
+            },
+          ],
+          yAxes: [
+            {
+              stacked: true,
+              ticks: {
+                // Include a dollar sign in the ticks
+                callback: function (value) {
+                  return "$" + value;
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+    this.setState({ chart: myChart });
   }
 
-  callback = (graphData) => {
-    console.log(graphData);
-  };
-
-  updateChart = (chart, label, data) => {};
-
-  handleClick = () => {
-    console.log("clicked");
+  updateChart = (chart) => {
+    chart.data.datasets[0].data = [100, 100];
+    chart.update();
+    // console.log(chart.data.datasets);
   };
 
   render() {
     return (
       <div className="sidenav" data={dummyData}>
-        <Graph />
-        <button onClick={this.handleClick()}>Update</button>
+        <div class="chart-container">
+          <canvas ref={this.chartRef} />
+        </div>
+        <button
+          onClick={() => {
+            this.updateChart(this.state.chart);
+          }}
+        >
+          Update
+        </button>
       </div>
     );
   }
