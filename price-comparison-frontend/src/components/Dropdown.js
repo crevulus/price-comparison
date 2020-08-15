f4b7a3864b1fbfc8307fdca7b5b3cd4f8c463453
crevulus <crevulus@gmail.com>
2020-08-15 16:50:51 +0200

    dropdown

--- price-comparison-frontend/src/components/Dropdown.js
+++ price-comparison-frontend/src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
+import "../styles/dropdown.css";
 
 export default class Card extends Component {
   constructor(props) {
@@ -12,25 +13,40 @@ export default class Card extends Component {
     this.showMenu = this.showMenu.bind(this);
   }
 
-  showMenu(event) {
+  showMenu = (event) => {
     event.preventDefault();
     this.setState((prevState) => ({
       showMenu: !prevState.showMenu,
     }));
-  }
+  };
+
+  handleClick = (e) => {
+    console.log(e.target.value);
+  };
 
   render() {
     return (
       <div className="question-block">
         <p>Choose your location</p>
-        <button onClick={this.showMenu}>Show menu</button>
-        {this.state.showMenu ? (
-          <div>
-            {this.props.locationNames.map((location) => {
-              return <button>{location.Location_name}</button>;
-            })}
-          </div>
-        ) : null}
+        <div className="dropdown">
+          <button onClick={this.showMenu} className="dropdown-btn">
+            Show menu
+          </button>
+          {this.state.showMenu ? (
+            <div className="dropdown-content">
+              {this.props.locationNames.map((location) => {
+                return (
+                  <button
+                    value={location.Location_name}
+                    onClick={this.handleClick}
+                  >
+                    {location.Location_name}
+                  </button>
+                );
+              })}
+            </div>
+          ) : null}
+        </div>
       </div>
     );
   }
