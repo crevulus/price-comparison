825d3d4745ebe0a337ea8623e35558e5867c25b7
crevulus <crevulus@gmail.com>
2020-09-22 12:45:15 +0200

    finished modals styling

--- price-comparison-frontend/src/components/Modals/CookieModal.js
+++ price-comparison-frontend/src/components/Modals/CookieModal.js
@@ -13,12 +13,14 @@ export default class CookieModal extends Component {
           Unfortunately, users who do not accept cookies are unable to use this
           website.
         </p>
-        <button className="submit" onClick={this.onClose}>
-          Accept
-        </button>
-        <a href="https://www.change-is.com/">
-          <button className="update">Back to Home</button>
-        </a>
+        <div className="btn-container">
+          <a href="https://www.change-is.com/">
+            <button className="update">Back to Home</button>
+          </a>
+          <button className="submit" onClick={this.onClose}>
+            Accept
+          </button>
+        </div>
       </div>
     );
   }
