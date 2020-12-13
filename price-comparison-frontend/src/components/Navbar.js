a0148fc1ae89beb8eb83c14f6174c3978f3c80ed
crevulus <crevulus@gmail.com>
2020-12-13 13:10:41 +0100

    navbar restyle

--- price-comparison-frontend/src/components/Navbar.js
+++ price-comparison-frontend/src/components/Navbar.js
@@ -3,8 +3,6 @@ import React from "react";
 import { ReactComponent as Logo } from "../assets/ChangeLogo.svg";
 import verloop from "../assets/verloop-01.png";
 
-import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-
 export default function Navbar() {
   return (
     <div className="navbar-container">
@@ -12,13 +10,15 @@ export default function Navbar() {
         <img className="verloop" src={verloop} alt="verloop" />
       </div>
       <div className="navbar-contents">
-        <a href="https://www.change-is.com/" className="back-link">
-          <button className="back-button">
-            <ArrowBackIcon />
-            <p className="back-text">Back</p>
-          </button>
-        </a>
         <Logo className="logo" />
+        <div className="navbar-buttons">
+          <a href="https://www.change-is.com/" className="back-link">
+            <button className="back-button">GO BACK</button>
+          </a>
+          <button className="login-button" disabled={true}>
+            LOGIN
+          </button>
+        </div>
       </div>
     </div>
   );
